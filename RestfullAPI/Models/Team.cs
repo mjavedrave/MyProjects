e7b2102411866f3e53a0b210528b61e77c1a347d
mjavedrave <mohd.javed@northgate-is.com>
2015-07-08 15:06:44 +0530

    my first commit

---
+++ RestfullAPI/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestfullAPI.Models
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public int PMID { get; set; }
+    }
+}
