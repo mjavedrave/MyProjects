419a9f137fc1a3324db27f7800583b273e06a85b
mjavedrave <mohd.javed@northgate-is.com>
2015-08-17 17:32:06 +0530

    added employee api

---
+++ RestfullAPI/Controllers/EmployeeApiController.cs
@@ -0,0 +1,44 @@
+﻿using RestfullAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestfullAPI.Controllers
+{
+    public class EmployeeApiController : ApiController
+    {
+        // GET api/employeeapi
+        public IEnumerable<Team> GetTeamList()
+        {
+            List<Team> lstTeam = new List<Team>();
+
+            Team team = new Team();
+            team.TeamID = 1;
+            team.TeamName = "Inspec";
+            lstTeam.Add(team);
+            team = new Team();
+            team.TeamID = 2;
+            team.TeamName = "IET ES+";
+            lstTeam.Add(team);
+            team = new Team();
+            team.TeamID = 3;
+            team.TeamName = "IET TV";
+            lstTeam.Add(team);
+            return lstTeam;
+        }
+
+        // GET api/employeeapi/5
+        public Team Get(int id)
+        {
+            Team team = new Team();
+            team.TeamID = 1;
+            team.TeamName = "Inspec";
+            return team;
+        }
+
+     
+    }
+}
